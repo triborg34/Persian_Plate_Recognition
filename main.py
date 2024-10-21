620cf4b187cd4dc5764c01ffa33a373a0bee33b9
triborg34 <aref.mousavi0914@gmail.com>
2024-10-21 10:38:34 +0330

    working yolov8

--- main.py
+++ main.py
@@ -3,113 +3,101 @@ import cv2
 import math
 import time
 import datetime
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
 
 # Get the current timestamp for output names
 timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-# Define the file name with the timestamp
 file_name = f'output_{timestamp}.jpg'
 classnames = ['car', 'plate']
-charclassnames = ['0','9','b','d','ein','ein','g','gh','h','n','s','1','malul','n','s','sad','t','ta','v','y','2'
-                  ,'3','4','5','6','7','8']
+charclassnames = ['0', '9', 'b', 'd', 'ein', 'ein', 'g', 'gh', 'h', 'n', 's', '1', 'malul', 'n', 's', 'sad', 't', 'ta', 'v', 'y', '2', '3', '4', '5', '6', '7', '8']
 
-# Connect to your IP camera using the RTSP stream
-source = "rtsp://username:password@camera_ip_address:554/stream"  # Replace this with your IP camera's RTSP URL
+# Set the RTSP stream source
+source = "rtsp://admin:admin@192.168.1.88:554/substream"  # Replace with your RTSP URL
 
-# Load YOLOv8 model
+# Load YOLOv8 models for object and character detection
+
 model_object = YOLO("weights/best.pt")
 model_char = YOLO("weights/yolov8n_char_new.pt")
 
-# Open the RTSP stream
 cap = cv2.VideoCapture(source)
+cap.set(cv2.CAP_PROP_BUFFERSIZE, 100)
+cap.set(cv2.CAP_PROP_POS_FRAMES, 10)
 
 # Check if the stream is opened successfully
 if not cap.isOpened():
     print("Error: Could not open the RTSP stream.")
     exit()
 
-# Define the output video properties
-output_videoname = f'output_{timestamp}.mp4'
+# Define the output video properties (optional if saving images)
 output_imagename = f'output_{timestamp}.jpg'
-fourcc = cv2.VideoWriter_fourcc(*"mp4v")
-fps = cap.get(cv2.CAP_PROP_FPS)
-frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
-if total_frames > 1:
-    video_writer = cv2.VideoWriter('output/' + output_videoname, fourcc, fps, (frame_width, frame_height))
+while cap.isOpened():
+    success, img = cap.read()
+    if success:
+        tick = time.time()
 
-    # Do inference on video from the RTSP stream
-    while cap.isOpened():
-        success, img = cap.read()
-        if success:
-            # Detect objects with yolov8 model
-            tick = time.time()
-            output = model_object(img, show=False, conf=0.7, stream=True)
+        # Detect objects with YOLO model
+        output = model_object(img, show=False, conf=0.7, stream=True)
 
-            # Extract bounding box and class names
-            for i in output:
-                bbox = i.boxes
-                for box in bbox:
-                    x1, y1, x2, y2 = box.xyxy[0]
-                    x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-                    cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), 3)
-                    confs = math.ceil((box.conf[0] * 100)) / 100
-                    cls_names = int(box.cls[0])
+        for result in output:
+            for box in result.boxes:
+                x1, y1, x2, y2 = map(int, box.xyxy[0].cpu().numpy())  # Convert to integers
+                cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
-                    # Add class-specific text (confidence values)
-                    if cls_names == 1:
-                        cv2.putText(img, f'{confs}', (max(40, x2 + 5), max(40, y2 + 5)), fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=0.5, color=(0, 20, 255), thickness=1, lineType=cv2.LINE_AA)
-                    elif cls_names == 0:
-                        cv2.putText(img, f'{confs}', (max(40, x1), max(40, y1)), fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=0.6, color=(0, 20, 255), thickness=1, lineType=cv2.LINE_AA)
+                confs = math.ceil((box.conf[0] * 100)) / 100
+                cls_names = int(box.cls[0])
 
-                    # Check plate to recognize characters
-                    if cls_names == 1:
-                        char_display = []
-                        plate_img = img[y1:y2, x1:x2]  # Crop plate from frame
-                        plate_output = model_char(plate_img, conf=0.3)  # Detect characters of plate
-                        
-                        # Extract bounding box and class names for characters
-                        bbox = plate_output[0].boxes.xyxy
-                        cls = plate_output[0].boxes.cls
+                if cls_names == 1:
+                    cv2.putText(img, f'{confs}', (max(40, x2 + 5), max(40, y2 + 5)), fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=0.5, color=(0, 20, 255), thickness=1, lineType=cv2.LINE_AA)
+                elif cls_names == 0:
+                    cv2.putText(img, f'{confs}', (max(40, x1), max(40, y1)), fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=0.6, color=(0, 20, 255), thickness=1, lineType=cv2.LINE_AA)
 
-                        # Sort characters from left to right
-                        keys = cls.cpu().numpy().astype(int)
-                        values = bbox[:, 0].cpu().numpy().astype(int)
-                        dictionary = list(zip(keys, values))
-                        sorted_list = sorted(dictionary, key=lambda x: x[1])
+                # Check for plates and recognize characters
+                if cls_names == 1:
+                    char_display = []
+                    plate_img = img[y1:y2, x1:x2]  # Crop plate from frame
+                    plate_output = model_char(plate_img, conf=0.3)
 
-                        for i in sorted_list:
-                            char_class = i[0]
-                            char_display.append(charclassnames[char_class])
+                    bbox = plate_output[0].boxes.xyxy
+                    cls = plate_output[0].boxes.cls
 
-                        char_result = 'Plate: ' + ''.join(char_display)
+                    # Sort characters left to right
+                    keys = cls.cpu().numpy().astype(int)
+                    values = bbox[:, 0].cpu().numpy().astype(int)
+                    dictionary = list(zip(keys, values))
+                    sorted_list = sorted(dictionary, key=lambda x: x[1])
 
-                        # Display detected plate characters
-                        if len(char_display) == 8:
-                            cv2.line(img, (max(40, x1 - 25), max(40, y1 - 10)), (x2 + 25, y1 - 10), (0, 0, 0), 20, lineType=cv2.LINE_AA)
-                            cv2.putText(img, char_result, (max(40, x1 - 15), max(40, y1 - 5)), fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=0.5, color=(10, 50, 255), thickness=1, lineType=cv2.LINE_AA)
+                    # Convert to string
+                    for i in sorted_list:
+                        char_class = i[0]
+                        char_display.append(charclassnames[char_class])
 
-            # Display frame and FPS
-            tock = time.time()
-            elapsed_time = tock - tick
-            fps_text = "FPS: {:.2f}".format(1 / elapsed_time)
-            text_size, _ = cv2.getTextSize(fps_text, cv2.FONT_HERSHEY_SIMPLEX, 1, 2)
-            fps_text_loc = (frame_width - text_size[0] - 10, text_size[1] + 10)
-            cv2.putText(img, fps_text, fps_text_loc, fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=1, color=(10, 50, 255), thickness=2, lineType=cv2.LINE_AA)
+                    char_result = 'Plate: ' + ''.join(char_display)
 
-            cv2.imshow('detection', img)
-            video_writer.write(img)
+                    if len(char_display) == 8:
+                        cv2.line(img, (max(40, x1 - 25), max(40, y1 - 10)), (x2 + 25, y1 - 10), (0, 0, 0), 20, lineType=cv2.LINE_AA)
+                        cv2.putText(img, char_result, (max(40, x1 - 15), max(40, y1 - 5)), fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=0.5, color=(10, 50, 255), thickness=1, lineType=cv2.LINE_AA)
 
-            # Break the loop if 'q' is pressed
-            if cv2.waitKey(1) & 0xFF == ord("q"):
-                break
-        else:
+        tock = time.time()
+        elapsed_time = tock - tick
+        fps_text = "FPS: {:.2f}".format(1 / elapsed_time)
+        text_size, _ = cv2.getTextSize(fps_text, cv2.FONT_HERSHEY_SIMPLEX, 1, 2)
+        fps_text_loc = (img.shape[1] - text_size[0] - 10, text_size[1] + 10)
+        cv2.putText(img, fps_text, fps_text_loc, fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=1, color=(10, 50, 255), thickness=2, lineType=cv2.LINE_AA)
+
+        # Show detection
+        cv2.imshow('detection', img)
+
+        # Save the frame as an image
+        cv2.imwrite('output/' + output_imagename, img)
+
+        # Break loop on 'q' key press
+        if cv2.waitKey(1) & 0xFF == ord("q"):
             break
+    else:
+        print("Error: Could not get any frames from the RTSP stream.")
+        break
 
-    cap.release()
-    video_writer.release()
-    cv2.destroyAllWindows()
-
-else:
-    print("Error: Could not get any frames from the RTSP stream.")
+cap.release()
+cv2.destroyAllWindows()
